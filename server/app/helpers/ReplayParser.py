ab4447289e9d09454458702409eece05ee660670
y <y@rq.local>
2020-08-24 22:33:44 +0300

    parsing sc2 replays methods added

--- server/app/helpers/ReplayParser.py
+++ server/app/helpers/ReplayParser.py
@@ -38,11 +38,55 @@ class ReplayParser:
         for i in tumors:
             print('{:>4}. {}'.format(i[0], 'tumor by Queen' if 'Build' not in i[1] else 'tumor by tumor'))
 
+    def _stats_hydra_lur_born_and_dead_time(self):
+        born_hydra = [(i.second, i.unit_type_name) for i in self.replay.events
+                      if i.name == 'UnitBornEvent' and i.unit_type_name == 'Hydralisk'
+                      and i.unit_upkeeper.name == 'yehor']
+        born_lur = [(i.second, i.unit_type_name) for i in self.replay.events
+                    if i.name == 'UnitTypeChangeEvent'
+                    and 'Lurker' == [j.name for j in i.unit.type_history.values()][-1]
+                    and i.unit.owner.name == 'yehor']  # TODO: need to debug as too many lurs
+        dead = [(i.second, i.unit.name) for i in self.replay.events
+                if i.name == 'UnitDiedEvent' and i.unit.name in ('Hydralisk', 'Lurker')
+                and i.unit.owner.name == 'yehor']
+        print('born hydra', born_hydra)
+        print('born lur', born_lur)
+        print('dead hydra and lur', dead)
+
+    def _stats_buildings_and_creep_init_time(self):
+        init = [(i.second, i.unit.name) for i in self.replay.events
+                if i.name == 'UnitInitEvent' and i.unit.owner.name == 'yehor']
+        print(init)
+
+    def _stats_buildings_and_creep_ready_time(self):
+        done = [(i.second, i.unit.name) for i in self.replay.events
+                if i.name == 'UnitDoneEvent' and i.unit.owner.name == 'yehor']
+        print(done)
+
+    def _stats_upgrades_ready_time(self):
+        upg_names = ('zerglingmovementspeed',
+                     'ZergMissileWeaponsLevel1', 'ZergMissileWeaponsLevel2',
+                     'EvolveMuscularAugments', 'EvolveGroovedSpines',
+                     'ZergGroundArmorsLevel1', 'ZergGroundArmorsLevel2', 'ZergGroundArmorsLevel3',
+                     'ZergMissileWeaponsLevel3',
+                     'overlordspeed', 'ZergFlyerWeaponsLevel1')
+        done = [(i.second, i.upgrade_type_name) for i in self.replay.events
+                if i.name == 'UpgradeCompleteEvent' and i.upgrade_type_name in upg_names
+                and i.player.name == 'yehor']
+        print(done)
+
     def parse(self):
         print(self.replay.game_length.total_seconds(), "seconds")  # 982
         print(self.replay.frames, "frames")  # 2201
         '''
         TODOs:
+        1. Decide how to visualize, e.g.: https://observablehq.com/@d3/gallery .
+        2. Prepare necessary charts (tutor: https://observablehq.com/@d3/learn-d3).
+        3. Store data to Pg to avoid parsing all replays to show performance comparison.
+        4. Integrate solution and put it to rqdev.com
+        5. Automate code delivery to prod.
+        
+        VISUALIZE:
         # army
         1. hydras + 2 * lurkers num in time        
         
@@ -75,5 +119,8 @@ class ReplayParser:
         21. spores ready
         22. metabolic boost ready        
         '''
-
+        self._stats_hydra_lur_born_and_dead_time()
+        self._stats_buildings_and_creep_init_time()
+        self._stats_buildings_and_creep_ready_time()
+        self._stats_upgrades_ready_time()
         self._stats_spread_creep()
